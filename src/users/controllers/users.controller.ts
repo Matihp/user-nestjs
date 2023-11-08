53e6e368a16375e58555da78f72768e57dcf3a23
matihp <matiasleoncontreras@gmail.com>
2023-11-08 20:01:53 -0300

    add roles decorator

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -3,19 +3,25 @@ import { UsersService } from '../services/users.service';
 import { UserDTO, UserToProjectDTO, UserUpdateDTO } from '../dto/user.dto';
 import { PublicAccess } from 'src/auth/decorators/public.decorator';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { RolesAccess } from 'src/auth/decorators/roles.decorator';
+import { AdminAccess } from 'src/auth/decorators/admin.decorator';
 
 @Controller('app')
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard,RolesGuard)
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
   @Post('register')
   public async RegisterUSer(@Body()body: UserDTO){
     return  await this.userService.createUser(body)
   }
+
+  @RolesAccess('ADMIN')
   @Get('all')
   public async findAllUsers(){
     return await this.userService.findUsers()
   }
+
   @PublicAccess()
   @Get(':id')
   public async findUserById(@Param('id') id:string){
