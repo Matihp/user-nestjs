fe7d4cc5e6bda0072b50b197cc1e511108f90604
matihp <matiasleoncontreras@gmail.com>
2023-11-13 23:26:38 -0300

    add access decorator

---
+++ src/auth/decorators/access-level.decorator.ts
@@ -0,0 +1,4 @@
+import { SetMetadata } from "@nestjs/common";
+import { ACCESS_LEVEL_KEY} from "src/constants/key.decorator";
+
+export const AccessLevel = (level:number)=>SetMetadata(ACCESS_LEVEL_KEY,level)
